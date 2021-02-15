850480ce4964ac17390d70dd3108afe9b53fe999
Stiward_Solano <13769238+StiwardSolano@users.noreply.github.com>
2021-02-15 16:59:14 -0500

    API updated

--- src/components/predictions.component.js
+++ src/components/predictions.component.js
@@ -81,7 +81,7 @@ function CreatePrediction() {
     };
 
     console.log(predictions);
-    axios.post('http://127.0.0.1:5000/pall', predictions)
+    axios.post('https://mongopy-api.herokuapp.com/pall', predictions)
     .then(res => {
       const predictive = JSON.parse(res.data.result);
       console.log(predictive);
