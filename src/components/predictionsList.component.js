d6bd191e73bf3bbb68ab80332b8e05557c3cc0f6
Stiward_Solano <13769238+StiwardSolano@users.noreply.github.com>
2021-02-15 16:58:32 -0500

    API updated

--- src/components/predictionsList.component.js
+++ src/components/predictionsList.component.js
@@ -12,7 +12,7 @@ function ListPredictions() {
   ]);
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:5000/predictions')
+    axios.get('https://mongopy-api.herokuapp.com/predictions')
     .then(response => {
      setPredictions(response.data);
     })
